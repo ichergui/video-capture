5483e7a85027748e0f2156b1b8b867eae77ede6d
Ilies CHERGUI <ilies.chergui@gmail.com>
2023-05-29 18:16:19 +0100

    video-capture: use h264_webcam_stream instead of linuxvideo
    Signed-off-by: Ilies CHERGUI <ilies.chergui@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-//! Prints detailed device information.
-
-use std::{env, path::Path};
+use std::{env, io::Write, path::Path};
 use anyhow::anyhow;
-use linuxvideo::Device;
 
 fn main() -> anyhow::Result<()> {
-    env_logger::init();
-
     let mut args = env::args_os().skip(1);
-    let path = args.next().ok_or_else(|| anyhow!("usage: info <device>"))?;
-    let device = Device::open(Path::new(&path))?;
-    let caps = device.capabilities()?;
+    
+    let device_path_str = args
+        .next()
+        .ok_or_else(|| anyhow!("usage: video-capture <device> <file>"))?;
 
-    println!("card: {}", caps.card());
-    println!("driver: {}", caps.driver());
-    println!("bus info: {}", caps.bus_info());
-    println!("all capabilities:    {:?}", caps.all_capabilities());
-    println!("avail. capabilities: {:?}", caps.device_capabilities());
+    let file_path = args
+        .next()
+        .ok_or_else(|| anyhow!("usage: video-capture <device> <file>"))?;
+
+    let max_fps = 30;
+
+    let device_path = Path::new(&device_path_str);
+    let mut device = h264_webcam_stream::get_device(&device_path)?;
+    let mut stream = h264_webcam_stream::stream(&mut device, max_fps)?;
+
+    let mut file = std::fs::File::create(file_path)?;
+
+    for _ in 0..60 {
+        let (h264_bytes, _) = stream.next(false)?;
+        // Record the h264 video to a file
+        file.write_all(&h264_bytes[..])?;
+    }
 
     Ok(())
+
 }
